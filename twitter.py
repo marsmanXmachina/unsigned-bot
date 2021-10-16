51b12d71c655911b68a9a7f5278549a091524cf7
MarsMan <mercuriusxmachina@posteo.de>
2021-10-16 08:52:29 +0200

    Removed animated tweet temporarily

--- twitter.py
+++ twitter.py
@@ -4,6 +4,8 @@ import time
 import tweepy
 import requests
 
+import asyncio
+
 from emojis import *
 
 
@@ -49,13 +51,17 @@ async def tweet_sales(api, sales):
 
             await gen_animation(str(unsig_number))
             filepath = f"img/animation_{unsig_number}.gif"
-            media = api.media_upload(filepath)
+            media = api.media_upload(filename=filepath, chunked=True)
+            api.update_status(status=tweet_string, media_ids=[media.media_id])
+
+            delete_image_files(IMAGE_PATH, suffix="gif")
+            
         except:
             tweet_string += f"\n{unsig_url}"
             api.update_status(status=tweet_string)
-        else:
-            api.update_status(status=tweet_string, media_ids=[media.media_id])
+
             delete_image_files(IMAGE_PATH, suffix="gif")
+
 
 def download_image(num, url) -> str:
     filename = f'temp_{num}.png'
@@ -67,3 +73,15 @@ def download_image(num, url) -> str:
     
     return filename
 
+# if __name__ == "__main__":
+
+#     twitter_api = create_twitter_api()
+
+#     sales = [
+# {"assetid": "unsig_13751", "unit": "0e14267a8020229adc0184dd25fa3174c3f7d6caadcb4425c70e7c04756e7369673133373531", "price": 992000000, "sold": True, "id": "614bdecc57294040c06ff657", "date": 1634348080120, "num_props": 5},
+# {"assetid": "unsig_24346", "unit": "0e14267a8020229adc0184dd25fa3174c3f7d6caadcb4425c70e7c04756e7369673234333436", "price": 1000000000, "sold": True, "id": "616a1758ddc02787669337ca", "date": 1634363294132, "num_props": 5}, 
+# {"assetid": "unsig_30189", "unit": "0e14267a8020229adc0184dd25fa3174c3f7d6caadcb4425c70e7c04756e7369673330313839", "price": 1000000000, "sold": True, "id": "616a17531c651312b0f4f153", "date": 1634363094784, "num_props": 6},
+# {"assetid": "unsig_30167", "unit": "0e14267a8020229adc0184dd25fa3174c3f7d6caadcb4425c70e7c04756e7369673330313637", "price": 1400000000, "sold": True, "id": "616654418cd55f4d2a745c1a", "date": 1634364508534, "num_props": 6}
+#     ]
+
+#     asyncio.run(tweet_sales(twitter_api, sales))
